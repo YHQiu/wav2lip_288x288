f57ddc2e3d9a197ccd52eb437d8e7e4ed23b5974
qiuhongyu <qiuhongyu@taoliweilai.com>
2024-05-08 15:59:57 +0800

    feat

--- train_syncnet_sam.py
+++ train_syncnet_sam.py
@@ -170,6 +170,7 @@ class Dataset(object):
                     with open(mel_out_path, "wb") as f:
                         np.save(f, orig_mel)
             except Exception as e:
+                print(e)
                 # print("mel", vidname)
                 continue
             print("call 5")
