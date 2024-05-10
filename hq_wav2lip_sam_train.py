58e1b095ca06ba2b7b05232bf68141c651ba7fcf
qiuhongyu <qiuhongyu@taoliweilai.com>
2024-05-10 15:26:48 +0800

    feat

--- hq_wav2lip_sam_train.py
+++ hq_wav2lip_sam_train.py
@@ -51,7 +51,8 @@ print('use_cuda: {}'.format(use_cuda))
 syncnet_T = 5
 syncnet_mel_step_size = 16
 disc_iter_start = 30000
-sync_iter_start = 250000
+# sync_iter_start = 250000
+sync_iter_start = 500
 hparams.set_hparam('img_size', 384)
 
 
